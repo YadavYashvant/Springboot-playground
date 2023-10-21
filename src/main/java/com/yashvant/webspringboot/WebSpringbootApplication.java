bf6d6816985f63c9d5072683d9b27d2cf4bbafe5
Yashvant Yadav <yashvantyadav855@gmail.com>
2023-10-21 13:20:04 +0530

    using beans in springboot

--- src/main/java/com/yashvant/webspringboot/WebSpringbootApplication.java
+++ src/main/java/com/yashvant/webspringboot/WebSpringbootApplication.java
@@ -1,7 +1,12 @@
 package com.yashvant.webspringboot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class WebSpringbootApplication {
@@ -10,4 +15,17 @@ public class WebSpringbootApplication {
 		SpringApplication.run(WebSpringbootApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("Let's inspect the beans provided by spring boot: ");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+
+			for (String beanName: beanNames) {
+				System.out.println(beanName);
+			}
+		};
+	}
 }
